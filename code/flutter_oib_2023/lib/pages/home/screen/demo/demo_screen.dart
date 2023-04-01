26aa0639004b3a5f4fecfaa9a44e9a57ccc66117
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-04-01 11:34:16 +0200

    feat: Add new card in the demo screen

--- code/flutter_oib_2023/lib/pages/home/screen/demo/demo_screen.dart
+++ code/flutter_oib_2023/lib/pages/home/screen/demo/demo_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_prefixes
 
 import 'package:flutter/material.dart';
+import 'package:flutter_oib_2023/ui/documentation_demo_card.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DemoScreen extends ConsumerWidget {
@@ -10,7 +11,18 @@ class DemoScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: ListView(),
+      child: ListView(
+        children: const [
+          DocumentationDemoCard(
+            title: "Syncfusion Flutter Widgets",
+            description:
+                "The Syncfusion Flutter Widgets are a set of advanced custom widgets and file formats packages needed to create rich and high-quality cross-platform applications in iOS, Android, and Web using a single code base.",
+            linkDocumentation:
+                "https://pub.dev/publishers/syncfusion.com/packages",
+            linkDemo: "https://flutter.syncfusion.com/#/",
+          ),
+        ],
+      ),
     );
   }
 }
