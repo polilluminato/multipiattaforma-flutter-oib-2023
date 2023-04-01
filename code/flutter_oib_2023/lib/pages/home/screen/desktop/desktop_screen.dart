c09ce62116f9f127edaca3f79888732ce44d223e
Alberto Bonacina <bonacina.alberto@gmail.com>
2023-04-01 11:34:00 +0200

    feat: Add new widget for the demo documentation card

--- code/flutter_oib_2023/lib/pages/home/screen/desktop/desktop_screen.dart
+++ code/flutter_oib_2023/lib/pages/home/screen/desktop/desktop_screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: library_prefixes
 
 import 'package:flutter/material.dart';
-import 'package:flutter_oib_2023/pages/home/screen/desktop/ui/desktop_card.dart';
+import 'package:flutter_oib_2023/ui/documentation_demo_card.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DesktopScreen extends ConsumerWidget {
@@ -13,38 +13,28 @@ class DesktopScreen extends ConsumerWidget {
       padding: const EdgeInsets.all(8),
       child: ListView(
         children: const [
-          DesktopCard(
+          DocumentationDemoCard(
             title: "Windows (fluent_ui)",
             description:
                 "Implements Windows UI in Flutter. Based on the official documentation",
             linkDocumentation: "https://pub.dev/packages/fluent_ui",
             linkDemo: "https://bdlukaa.github.io/fluent_ui/",
           ),
-          SizedBox(
-            height: 16,
-          ),
-          DesktopCard(
+          DocumentationDemoCard(
             title: "macOS (macos_ui)",
             description:
                 "Flutter widgets and themes implementing the current macOS design language.",
             linkDocumentation: "https://pub.dev/packages/macos_ui",
             linkDemo: "https://macosui.dev/",
           ),
-          SizedBox(
-            height: 16,
-          ),
-          DesktopCard(
+          DocumentationDemoCard(
             title: "Linux/Ubuntu (yaru)",
             description:
                 "Ubuntu Yaru Style - Distinct look and feel of the Ubuntu Desktop",
             linkDocumentation: "https://pub.dev/packages/yaru",
             linkDemo: "https://ubuntu.github.io/yaru.dart",
           ),
-          SizedBox(
-            //yaru_widgets
-            height: 16,
-          ),
-          DesktopCard(
+          DocumentationDemoCard(
             title: "Linux/Ubuntu (yaru_widgets)",
             description:
                 "Common flutter widgets useful for building desktop and web applications",
--- code/flutter_oib_2023/lib/ui/documentation_demo_card.dart
+++ code/flutter_oib_2023/lib/ui/documentation_demo_card.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: library_prefixes
+
 import 'package:flutter/material.dart';
 import 'package:flutter_oib_2023/utils/platform_utils.dart' as PlatformUtils;
 
-class DesktopCard extends StatelessWidget {
-  const DesktopCard(
+class DocumentationDemoCard extends StatelessWidget {
+  const DocumentationDemoCard(
       {super.key,
       required this.title,
       required this.description,
@@ -19,8 +21,10 @@ class DesktopCard extends StatelessWidget {
     TextTheme textTheme = Theme.of(context).textTheme;
 
     return Card(
+      margin: const EdgeInsets.only(bottom: 16, left: 8, right: 8),
       child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding:
+            const EdgeInsets.only(left: 16, right: 16, bottom: 12, top: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -31,6 +35,9 @@ class DesktopCard extends StatelessWidget {
             Text(
               description,
               style: textTheme.bodyMedium,
+            ),
+            const SizedBox(
+              height: 8,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
